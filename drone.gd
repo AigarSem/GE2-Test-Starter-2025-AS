0f08d51894a051c117532642a7039cfeac9e4515
AigarSem <C21322311@mytudublin.ie>
2025-04-29 10:23:21 +0100

    Added Drone Prop Spinning

--- drone.gd
+++ drone.gd
@@ -2,10 +2,13 @@ extends RigidBody3D
 
 @export var m:float = 20
 @export var max_speed:float = 25
-@export var spin_up_speed:float = 0.2
+@export var spin_up_speed:float = 1.5
+@export var slow_down_speed:float = 0.25
 @export var turn_speed:float = 5.0
 
-var moveUp:bool = false
+@onready var props:Array = [$Arm1/Prop, $Arm2/Prop, $Arm3/Prop, $Arm4/Prop]
+
+var current_turn_speed:float = 0.0
 
 
 func _physics_process(delta: float) -> void:
@@ -15,17 +18,22 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_pressed("up"):		
 		linear_velocity += (max_speed * up * delta)
+		current_turn_speed += spin_up_speed
+		spin_props()
+	else:
+		if current_turn_speed > 0:
+			current_turn_speed -= slow_down_speed
+			spin_props()
 	if Input.is_action_pressed("forward"):
 		linear_velocity += (max_speed * forward * delta)
 	if Input.is_action_pressed("reverse"):
 		linear_velocity += (max_speed * reverse * delta)
-	
 	if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
-		
 		var direction = Input.get_axis("left", "right")
 		
 		rotate(Vector3.UP, deg_to_rad(turn_speed) * -direction)
 
 
-func moveUpDrone() -> void:
-	moveUp = true
+func spin_props() -> void:
+	for prop in props:
+		prop.rotate(Vector3.UP, deg_to_rad(current_turn_speed))
