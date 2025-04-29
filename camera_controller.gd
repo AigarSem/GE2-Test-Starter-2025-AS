dd08f818d0460a6369dc630e989f85cfe25fea1e
AigarSem <C21322311@mytudublin.ie>
2025-04-29 10:50:11 +0100

    Added Optional Feature of First Person Camera for Drone

--- camera_controller.gd
+++ camera_controller.gd
@@ -2,8 +2,23 @@ extends Camera3D
 
 @export var drone:RigidBody3D
 @export var target:Marker3D
+@export var targetFPV:Marker3D
+# Target for the FPV Camera since I cant figgure out how to face forward :)
+@export var targetFPVTarget:Marker3D
 
+# Camera Modes true = Follow / false = First Person Camera
+# Press C to change Camera Mode
+var camera_mode:bool = true
 
 func _process(delta: float) -> void:
-	global_position = lerp(global_position, target.global_position, delta * 10.0)
-	look_at(drone.global_position)
+	if camera_mode:
+		global_position = lerp(global_position, target.global_position, delta * 10.0)
+		look_at(drone.global_position)
+	else:
+		global_position = targetFPV.global_position
+		look_at(targetFPVTarget.global_position)
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("camera_mode_change"):
+		camera_mode = !camera_mode
